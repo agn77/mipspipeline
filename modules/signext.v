5d34ce8f58b87d111809544527f2a9030e124e61
agn77 <53025590+agn77@users.noreply.github.com>
2020-02-11 21:23:45 -0800

    Update signext.v

--- modules/signext.v
+++ modules/signext.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:57:32 02/02/2020 
-// Design Name: 
-// Module Name:    signext 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module signext(
 	input [15:0]x,
 	output [31:0]y
